d2220db3b13b5482f388a045ef0cce102d93baa8
RonildoSilvaOSF <83358803+RonildoSilvaOSF@users.noreply.github.com>
2021-12-07 11:10:30 -0300

    Projeto em branco + configuração Firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { environment } from './../environments/environment';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
